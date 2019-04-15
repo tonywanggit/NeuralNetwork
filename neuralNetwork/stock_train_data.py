1ea6a317b073e2807c2a954e698be0fd68420344
王旭东 <wangxudong@ymatou.com>
2019-04-15 21:53:53 +0800

    init input data array

--- neuralNetwork/stock_train_data.py
+++ neuralNetwork/stock_train_data.py
@@ -10,13 +10,18 @@ if __name__ == '__main__':
     hidden_nodes = 100
     output_nodes = 1
     learning_rate = 0.2
+    stock_max_row = 10000
 
     db = create_engine('mysql+pymysql://root:111111@172.16.100.173:3306/neuralnetwork?charset=utf8')
-    sql_cmd = 'select open, close, high, low from stock_basic order by trade_date asc limit %d' % input_day
+    sql_cmd = 'select open, close, high, low from stock_basic order by trade_date asc limit %d' % stock_max_row
     df = pd.read_sql(sql=sql_cmd, con=db)
 
-    print(df)
-    print(df[2:4])
+    price_max = df['high'].max();
+    # vol_max = df['vol'].max();
+    # print(price_max, vol_max)
+
+    print((np.asfarray(df[0:7].values).reshape(28) / price_max * 0.99) + 0.01)
+
 
     neuralNetwork = NeuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
     print("run nerual network")
