625c18c6e29969f6f7e1d30ad6aa5eeb80644fd8
tony <wangxudong@ymatou.com>
2019-04-18 09:09:37 +0800

    添加测试集

---
+++ neuralNetwork/dash_board.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+if __name__ == '__main__':
+    np_float = float(19.0)
+    print(np_float >= 1)
+
+
--- neuralNetwork/stock_train_data.py
+++ neuralNetwork/stock_train_data.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import numpy as np
-import scipy
+import math
 from sqlalchemy import create_engine
 
 from neuralNetwork.neural_network import NeuralNetwork
 
-input_days = 5  # 取5天前的数据，预测2天后的最高值
-target_days = 2
+input_days = 5  # 取5天前的数据
+target_days = 2  # 预测2天后的最高值
+test_days = 2  # 测试天数
 input_nodes = input_days * 4  # 输入节点 = 输入天数 * 每天的数据量
 hidden_nodes = 100
 output_nodes = 1
@@ -16,21 +17,36 @@ neuralNetwork = NeuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_
 
 
 def train(input_data, target_data, price_max):
-    print(input_data)
-    print(input_data[4:5]['close'])
-    print(target_data)
-    print(target_data['high'].max())
-
-    last_close = input_data[input_days - 1:input_days]['close']
-    high_max = target_data['high'].max()
-    profit = high_max - last_close
-    profit_max = last_close * 0.1 * target_days  # 每天最多盈利 10%
-    target = profit / profit_max
+    last_close = input_data[input_days - 1:input_days]['close'].astype(np.float)
+    profit = target_data['high'].max() - last_close
 
     inputs = (np.asfarray(input_data.values).reshape(input_nodes) / price_max * 0.99) + 0.01
-    targets = np.asfarray(scipy.special.expit(target))
-    neuralNetwork.train(inputs, targets)
+    outputs = np.asfarray(cal_output(profit, last_close))
+
+    neuralNetwork.train(inputs, outputs)
     return
+
+
+def query(input_data, target_data, price_max):
+    print(input_data)
+    print(target_data)
+
+    inputs = (np.asfarray(input_data.values).reshape(input_nodes) / price_max * 0.99) + 0.01
+    target_value = neuralNetwork.query(inputs)
+
+    print(target_value)
+
+
+def cal_output(profit, last_close):
+    profit_max = last_close * math.pow(1.1, target_days) - last_close  # 每天最多盈利 10%
+    target = np.float(profit / profit_max)
+
+    if target >= 1:
+        return 0.99
+    elif target <= -1:
+        return 0.01
+    else:
+        return target * 0.5 + 0.5
 
 
 if __name__ == '__main__':
@@ -38,9 +54,13 @@ if __name__ == '__main__':
     sql_cmd = 'select open, close, high, low from stock_basic order by trade_date asc limit %d' % stock_max_row
     df = pd.read_sql(sql=sql_cmd, con=db)
 
-    price_max = df['high'].max()
-    data_size = df.size
+    high_max = df['high'].max()  # 全数据集的最高价
+    train_times = df.shape[0] - input_days - target_days + 1 - test_days  # 计算训练次数
+    if train_times <= 0:
+        exit("not enough data")
 
-    print()
+    for i in range(0, train_times):
+        train(df[i:input_days + i], df[input_days + i:input_days + target_days + i], high_max)
 
-    train(df[0:input_days], df[input_days:input_days + target_days], price_max)
+    for i in range(train_times, train_times + test_days):
+        query(df[i:input_days + i], df[input_days + i:input_days + target_days + i], high_max)
