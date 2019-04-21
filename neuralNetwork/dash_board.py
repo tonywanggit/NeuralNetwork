ce266a2ed12b53a9464b640a75e4ae67f2317f39
王旭东 <wangxudong@ymatou.com>
2019-04-21 21:12:08 +0800

    添加查询输出的图形展示

--- neuralNetwork/dash_board.py
+++ neuralNetwork/dash_board.py
@@ -1,7 +1,15 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
-if __name__ == '__main__':
-    np_float = float(19.0)
-    print(np_float >= 1)
+x = np.linspace(-1, 1, 50)
+y = 2*x + 1
+z = 2*x + 5
+
+plt.figure()
+plt.plot(x, y)
+plt.plot(x, z)
+plt.show()
+
+
 
 
--- neuralNetwork/stock_import_data.py
+++ neuralNetwork/stock_import_data.py
@@ -4,14 +4,15 @@ from sqlalchemy import create_engine
 if __name__ == '__main__':
     ts.set_token('E83fb68e2e009d139c1522bc6191100e9ab2a5933f19c25e49d04151')
     pro = ts.pro_api()
-    df = pro.trade_cal(exchange='', start_date='20180901', end_date='20181001', fields='exchange,cal_date,is_open,pretrade_date', is_open='0')
-
+    df = pro.trade_cal(exchange='', start_date='20150901', end_date='20190421', fields='exchange,cal_date,is_open,pretrade_date', is_open='0')
     stock_basic = pro.stock_basic(list_status='L')
 
     db = create_engine('mysql+pymysql://root:111111@172.16.100.173:3306/neuralnetwork?charset=utf8')
+    stock_basic.to_sql('stock_basic', db, index=False, if_exists='append')
 
-    daily = pro.daily(ts_code='002230.SZ', start_date='20180101', fields='ts_code,trade_date,open,close,pre_close,high,low,vol,amount')
-    daily.to_sql('stock_basic', db, index=False, if_exists='append')
+    # db = create_engine('mysql+pymysql://root:111111@172.16.100.173:3306/neuralnetwork?charset=utf8')
+    # daily = pro.daily(ts_code='002230.SZ', start_date='20150101', fields='ts_code,trade_date,open,close,pre_close,high,low,vol,amount')
+    # daily.to_sql('stock_daily', db, index=False, if_exists='append')
 
 
-    print(daily)
+    print(stock_basic)
